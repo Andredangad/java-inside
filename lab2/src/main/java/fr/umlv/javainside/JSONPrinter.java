1ec72423f5cfd6907d6ea4e204c7fa0373115a32
Andre-Marie Dang <andre.dang@hotmail.fr>
2020-09-29 10:38:27 +0200

    modify toJson using invoke() and getAccessor()

--- lab2/src/main/java/fr/umlv/javainside/JSONPrinter.java
+++ lab2/src/main/java/fr/umlv/javainside/JSONPrinter.java
@@ -1,5 +1,9 @@
 package fr.umlv.javainside;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.RecordComponent;
+import java.lang.reflect.UndeclaredThrowableException;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -22,9 +26,29 @@ public class JSONPrinter {
       }
       """.formatted(alien.age(), alien.planet());
     } */
-
+    private static Object invokeAccessor(Method accessor, Record record){
+        try {
+            return accessor.invoke(record);
+        }
+        catch (IllegalAccessException e) {
+            throw (IllegalAccessError) new IllegalAccessError().initCause(e);
+        } catch (InvocationTargetException e) {
+            Throwable cause = e.getCause();
+            if(cause instanceof RuntimeException re){
+                throw re;
+            }
+            if(cause instanceof  Error error){
+                throw error;
+            }
+            throw new UndeclaredThrowableException(cause);
+        }
+    }
     public static String toJSON(Record record) {
-        return Arrays.stream(record.getClass().getRecordComponents()).map(e -> e.toString()).collect(Collectors.joining( " "));
+        // return Arrays.stream(record.getClass().getRecordComponents()).map(e -> e.toString()).collect(Collectors.joining( " "));
+        return Arrays.stream(record.getClass().getRecordComponents())
+                .map(RecordComponent::getAccessor).map(e ->e.getName() + " " + invokeAccessor(e, record))
+                .map(e -> e.toString())
+                .collect(Collectors.joining(" "));
     }
 
     public static void main(String[] args) {

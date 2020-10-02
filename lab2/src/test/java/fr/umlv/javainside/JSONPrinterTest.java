39957fbcb9267c86cc764a8c5ff90fefcb5b8dbf
Andre-Marie Dang <andre.dang@hotmail.fr>
2020-10-02 10:29:10 +0200

    Final project

--- lab2/src/test/java/fr/umlv/javainside/JSONPrinterTest.java
+++ lab2/src/test/java/fr/umlv/javainside/JSONPrinterTest.java
@@ -3,14 +3,32 @@ package fr.umlv.javainside;
 import org.junit.jupiter.api.Test;
 
 import static fr.umlv.javainside.IncompleteJSONParser.parse;
+import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.*;
 
-class JSONPrinterTest {
+public class JSONPrinterTest {
+    record Alien(int age, String planet) {
+        public Alien {
+            if (age < 0) {
+                throw new IllegalArgumentException("negative age");
+            }
+            requireNonNull(planet);
+        }
+    }
+    record Person( String firstName, String lastName) {
+        public Person {
+            requireNonNull(firstName);
+            requireNonNull(lastName);
+        }
+    }
+
+
     @Test
     public void json() {
         var person = new Person("John","Doe");
-        var JSONprint = new JSONPrinter();
-        assertEquals(parse("{ \"firstName\": \"John\", \"lastName\": \"Doe\" }"), parse(JSONprint.toJSON(person)));
+        var jsonPrint = new JSONPrinter();
+        assertEquals(parse("{ \"firstName\": \"John\", \"lastName\": \"Doe\" }"), parse(jsonPrint.toJSON(person)));
+
     }
 
 }

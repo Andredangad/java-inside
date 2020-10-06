baac0d10a71f74f9308980d121639089c56ec42e
Andre-Marie Dang <andre.dang@hotmail.fr>
2020-10-06 09:20:30 +0200

    Add MethodHandleTests

--- lab3/src/main/java/fr/umlv/javainside/Alien.java
+++
@@ -1,7 +0,0 @@
-package fr.umlv.javainside;
-
-import static java.util.Objects.requireNonNull;
-
-public record Alien(int age, String planet) {
-
-}
--- lab3/src/main/java/fr/umlv/javainside/Book.java
+++
@@ -1,3 +0,0 @@
-package fr.umlv.javainside;
-
-record Book(@JSONProperty("book-title") String title, int year) { }

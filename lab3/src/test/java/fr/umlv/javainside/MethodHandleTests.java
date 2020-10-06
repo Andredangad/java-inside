c17e502f29731050441a441483b8a6303aafcc60
Andre-Marie Dang <andre.dang@hotmail.fr>
2020-10-06 10:32:12 +0200

    Add invokeStaticTest and invokeVirtualTest

--- lab3/src/test/java/fr/umlv/javainside/MethodHandleTests.java
+++ lab3/src/test/java/fr/umlv/javainside/MethodHandleTests.java
@@ -1,12 +1,14 @@
 package fr.umlv.javainside;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.lang.invoke.MethodHandles;
-import java.util.Arrays;
+import java.lang.invoke.WrongMethodTypeException;
+
 
 import static java.lang.invoke.MethodType.methodType;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MethodHandleTests {
     @Test
@@ -41,9 +43,57 @@ public class MethodHandleTests {
     public void  invokeExactStaticWrongArgumentTest() throws Throwable {
         var lookup = MethodHandles.lookup();
         var mh = lookup.findStatic(Integer.class,
-                "parseInt", methodType(int.class));
-        var res = (int)mh.invokeExact("5");
-        assertEquals(5,res);
+                "parseInt", methodType(int.class,String.class));
+        assertThrows(WrongMethodTypeException.class, () -> mh.invokeExact(5));
+
+    }
+
+    @Test
+    public void invokeExactVirtualTest() throws Throwable {
+        var lookup = MethodHandles.lookup();
+        var mh = lookup.findVirtual(String.class,
+                "toUpperCase", methodType(String.class));
+        var res = (String)mh.invokeExact("a");
+        assertEquals("A",res);
+
+    }
+    @Test
+    public void invokeExactVirtualWrongArgumentTest() throws Throwable {
+        var lookup = MethodHandles.lookup();
+        var mh = lookup.findVirtual(String.class,
+                "toUpperCase", methodType(String.class));
+        var res = (String)mh.invokeExact("a");
+        assertThrows(WrongMethodTypeException.class, () -> mh.invokeExact(5));
+
+    }
+
+    @Test
+    public void invokeStaticTest() throws Throwable {
+        var lookup = MethodHandles.lookup();
+        var mh = lookup.findStatic(Integer.class,
+                "parseInt", methodType(int.class, String.class));
+        var res = (Integer)mh.invoke("5");
+        Assertions.assertAll(
+                () ->  assertEquals(5,res),
+                () -> assertThrows(WrongMethodTypeException.class, () -> {
+                    var s =(String) mh.invoke("5");
+                }));
+
+
+    }
+
+    @Test
+    public void invokeVirtualTest() throws Throwable {
+        var lookup = MethodHandles.lookup();
+        var mh = lookup.findVirtual(String.class,
+                "toUpperCase", methodType(String.class));
+        var res = (Object)mh.invoke("a");
+        assertThrows(WrongMethodTypeException.class, () -> mh.invoke(5));
+        Assertions.assertAll(
+                () ->  assertEquals("A",res),
+                () -> assertThrows(WrongMethodTypeException.class, () -> {
+                    var d = (Double) mh.invoke(5);
+                }));
 
     }
 }
